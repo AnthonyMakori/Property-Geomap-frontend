d9e1a827f42b7465bbb0b55b993cf3cd9bcf41fc
Enock Monyancha <enocmonyancha@gmail.com>
2023-09-01 19:54:40 +0300

    fix sidenav

--- routes/index.ts
+++ routes/index.ts
@@ -4,40 +4,23 @@ function path(root: string, sublink: string) {
 
 const ROOTS_DASHBOARD = '/dashboard'
 const ROOTS_PAGES = '/pages'
-const ROOTS_PROJECTS = '/projects'
-const ROOTS_ORDERS = '/orders'
 const ROOTS_INVOICES = '/invoices'
-const ROOTS_TASKS = '/tasks'
-const ROOTS_CALENDAR = '/calendar'
 const ROOTS_AUTH = '/auth'
 const ROOTS_ERRORS = ''
-const ROOTS_START = '/start'
-const ROOTS_DOCS = '/docs'
-const ROOTS_CHANGELOG = '/changelog'
 
 export const PATH_DASHBOARD = {
     root: ROOTS_DASHBOARD,
     default: path(ROOTS_DASHBOARD, '/default'),
-    analytics: path(ROOTS_DASHBOARD, '/analytics'),
-    saas: path(ROOTS_DASHBOARD, '/saas'),
+    analytics: path(ROOTS_DASHBOARD, '/askaris'),
+    saas: path(ROOTS_DASHBOARD, '/admin'),
 }
 
 export const PATH_PAGES = {
     root: ROOTS_PAGES,
     profile: path(ROOTS_PAGES, '/profile'),
     settings: path(ROOTS_PAGES, '/settings'),
-    pricing: path(ROOTS_PAGES, '/pricing'),
-    chat: path(ROOTS_PAGES, '/chat'),
-    blank: path(ROOTS_PAGES, '/blank'),
 }
 
-export const PATH_PROJECTS = {
-    root: ROOTS_PROJECTS,
-}
-
-export const PATH_ORDERS = {
-    root: ROOTS_ORDERS,
-}
 
 export const PATH_INVOICES = {
     root: ROOTS_INVOICES,
@@ -48,13 +31,6 @@ export const PATH_INVOICES = {
     }
 }
 
-export const PATH_TASKS = {
-    root: ROOTS_TASKS,
-}
-
-export const PATH_CALENDAR = {
-    root: ROOTS_CALENDAR,
-}
 
 export const PATH_AUTH = {
     root: ROOTS_AUTH,
@@ -79,7 +55,7 @@ export const PATH_DOCS = {
 }
 
 export const PATH_CHANGELOG = {
-    root: ROOTS_CHANGELOG,
+    root: '',
 }
 
 export const PATH_GITHUB = {
